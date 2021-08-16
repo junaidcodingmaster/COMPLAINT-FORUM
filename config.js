a6a2c852d0c1d62f815796f3511296ff4c8a1904
junaidcodingmaster <mohammedjunaidmaqsood8@gmail.com>
2021-08-16 15:01:33 +0530

    Made By Junaid

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8SgPAIR2xUvZRVYI77p--NT72mnOIP7s",
+  authDomain: "complaint-forum-aa6c9.firebaseapp.com",
+  projectId: "complaint-forum-aa6c9",
+  storageBucket: "complaint-forum-aa6c9.appspot.com",
+  messagingSenderId: "491609282234",
+  appId: "1:491609282234:web:bc4da34af0bb5f7b5dd483"
+};
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
---
+++ screens/WriteComplaint.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import db from '../config'
+import firebase from 'firebase';
+import AppHeader from '../assets/AppHeader';
+ 
+export default class WriteComplaint extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            student: '',
+            query: '',
+        }
+    }
+
+    submitQuery = ()=>{
+        db.collection("complaints").add({
+            title : this.state.title,
+            student: this.state.student,
+            query: this.state.query
+
+        })
+        this.setState({
+            title :'',
+            student:'',
+            query:''
+        })
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <AppHeader title="COMPLAINT FORUM" />
+                <TextInput 
+                    placeholder="Title"
+                    placeholderTextColor='black'
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            title: text
+                        })
+                    }}
+                    value={this.state.title}
+                    style={styles.title}/>
+                <TextInput
+                    placeholder="Student Name"
+                    placeholderTextColor='black'
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            student: text
+                        })
+                    }}
+                    value={this.state.student}
+                    style={styles.author} />
+                <TextInput 
+                    placeholder="Your Complaint/Query"
+                    placeholderTextColor='black'
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            query: text
+                        })
+                    }}
+                    value={this.state.query}
+                    style={styles.storyText}
+                    multiline={true}/>
+                
+                <TouchableOpacity
+                    style={styles.submitButton}
+                   onPress = {this.submitQuery}
+                   >
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  title:{
+      height: 40,
+      borderWidth: 2,
+      marginTop: 40,
+      margin: 10,
+      color:'black',
+      padding: 6,
+      borderRadius:15
+
+  },
+  author: {
+      height: 40,
+      borderWidth: 2,
+      margin: 10,
+       padding: 6,
+       borderRadius:15
+  },
+  storyText: {
+      height: 250,
+      borderWidth: 2,
+      margin: 10, 
+      padding: 6,
+      borderRadius:20
+  },
+  submitButton:{
+      justifyContent: 'center',
+      alignSelf: 'center',
+      backgroundColor: '#2C2CE3',
+      width: 100,
+      height: 40,
+      color:'black',
+      borderRadius:20,
+      marginTop:20
+  },
+  buttonText: {
+      textAlign: 'center',
+      color: 'white',
+      fontWeight: 'bold',
+  }
+});
